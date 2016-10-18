91f0934fe7165d589dfb7b4d4c0f55f7c8368bfe
M558758 <scott.le@macys.com>
2016-10-18 12:36:18 -0700

    fix viewport

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ const renderFullPage = (html, css, initialState) => {
     <html>
       <head>
         <style data-aphrodite>${css.content}</style>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
       </head>
